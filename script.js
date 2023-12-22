3911d08875214788f201a46abda286df3b2bc9da
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-22 23:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 	var firstName=document.getElementById('firstName').value;
 	var lastName=document.getElementById('lastName').value;
-	alert("First name:"+firstName+"\nLast name:"+ lastName);
+	alert(firstName+ lastName);
 }
-getFormvalue();
+
